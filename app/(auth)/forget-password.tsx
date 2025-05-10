e7597eedbfceba4bd40b35fa3af2170524bf78cb
AndyHsieh <andy.hsieh@gaiatechs.com>
2025-05-10 13:38:17 +0800

    project init

---
+++ app/(auth)/forget-password.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function ForgetPassword() {
+  return (
+    <View>
+      <Text>forget-password</Text>
+    </View>
+  )
+}
---
+++ app/(auth)/reset-passowrd.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function ResetPassword() {
+  return (
+    <View>
+      <Text>reset-passowrd</Text>
+    </View>
+  )
+}
